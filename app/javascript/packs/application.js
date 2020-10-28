ee231f0305e5965b2b646a87f319fe8632e4d87f
abrandao <brandao@rubiti.com>
2020-10-28 16:53:34 -0300

    invert button added and font-awesome installed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,7 @@ require("jquery")
 require("../channels/exchanges.coffee")
 
 import "bootstrap"
-//import '../channels/exchanges'
+import "@fortawesome/fontawesome-free/js/all"
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
